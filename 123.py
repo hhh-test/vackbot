3ab0749fc53d4a4235684a20854d94e5a229b0b4
zhaolei <zhaolei@vackbot.com>
2018-10-08 14:51:54 +0800

    update

--- 123.py
+++ 123.py
@@ -1,1 +1,18 @@
-这是一个测试hahahah
+File "/Users/lei/Desktop/desktop/component/vackbot/123.py", line 1
+SyntaxError: Non-ASCII character '\xe8' in file /Users/lei/Desktop/desktop/component/vackbot/123.py on line 1, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
+[Finished in 0.1s with exit code 1]
+[shell_cmd: "/Users/lei/.local/share/virtualenvs/dev-gufA1ozj/bin/python" -u "/Users/lei/Desktop/desktop/component/vackbot/123.py"]
+[dir: /Users/lei/Desktop/desktop/component/vackbot]
+[path: /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public]
+这是干扰
+this is a test vackbot
+ganrao
+ganrao
+这是干扰
+这是干扰这是干扰
+File "/Users/lei/Desktop/desktop/component/vackbot/123.py", line 1
+SyntaxError: Non-ASCII character '\xe8' in file /Users/lei/Desktop/desktop/component/vackbot/123.py on line 1, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
+[Finished in 0.1s with exit code 1]
+[shell_cmd: "/Users/lei/.local/share/virtualenvs/dev-gufA1ozj/bin/python" -u "/Users/lei/Desktop/desktop/component/vackbot/123.py"]
+[dir: /Users/lei/Desktop/desktop/component/vackbot]
+[path: /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public]
